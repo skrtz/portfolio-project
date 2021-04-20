2e46c347d2014e91fdcf78607e07992ccc1614c5
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-20 17:51:43 -0400

    restructure page

---
+++ assets/portfolio.js
@@ -0,0 +1,5 @@
+var home = document.getElementById('home');
+var aboutMe = document.getElementById('about-me');
+var myWork = document.getElementById('my-work')
+var contact = document.getElementById('contact-info');
+
