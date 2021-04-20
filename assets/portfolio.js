09cebb1a3865e2e2235901796192dc4b83dff75d
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-20 19:32:53 -0400

    update homepage

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,5 +1,28 @@
-var home = document.getElementById('home');
-var aboutMe = document.getElementById('about-me');
-var myWork = document.getElementById('my-work')
-var contact = document.getElementById('contact-info');
+var bioLink = document.getElementById('about-link');
+var workLink = document.getElementById('work-link');
+var contactLink = document.getElementById('contact-link');
 
+var homePage = document.getElementById('home');
+var bioPage = document.getElementById('about-me');
+var workPage = document.getElementById('my-work')
+var contactPage = document.getElementById('contact-info');
+
+
+    bioLink.addEventListener('click', function(){
+        bioPage.style.display = 'flex';
+        homePage.style.display = 'none';
+        workPage.style.display = 'none';
+        contactPage.style.display = 'none';
+    });
+    workLink.addEventListener('click', function(){
+        workPage.style.display = 'flex';
+        homePage.style.display = 'none';
+        bioPage.style.display = 'none';
+        contactPage.style.display = 'none';
+    });
+    contactLink.addEventListener('click', function(){
+        contactPage.style.display = 'flex';
+        homePage.style.display = 'none';
+        workPage.style.display = 'none';
+        bioPage.style.display = 'none';
+    });
