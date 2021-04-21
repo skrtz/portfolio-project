c3e8bfbab59b4552d803e6d9990a59131a171418
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-21 18:11:24 -0400

    replace out layout

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,35 +1,35 @@
-var bioLink = document.getElementById('about-link');
-var workLink = document.getElementById('work-link');
-var contactLink = document.getElementById('contact-link');
+// var bioLink = document.getElementById('about-link');
+// var workLink = document.getElementById('work-link');
+// var contactLink = document.getElementById('contact-link');
 
-var homePage = document.getElementById('home');
-var bioPage = document.getElementById('about-me');
-var workPage = document.getElementById('my-work')
-var contactPage = document.getElementById('contact-info');
-
-
-    bioLink.addEventListener('click', function(){
-        bioPage.style.display = 'flex';
-        // homePage.style.display = 'none';
-        workPage.style.display = 'none';
-        contactPage.style.display = 'none';
-    });
-    workLink.addEventListener('click', function(){
-        workPage.style.display = 'flex';
-        homePage.style.display = 'none';
-        bioPage.style.display = 'none';
-        contactPage.style.display = 'none';
-    });
-    contactLink.addEventListener('click', function(){
-        contactPage.style.display = 'flex';
-        homePage.style.display = 'none';
-        workPage.style.display = 'none';
-        bioPage.style.display = 'none';
-    });
+// var homePage = document.getElementById('home');
+// var bioPage = document.getElementById('about-me');
+// var workPage = document.getElementById('my-work')
+// var contactPage = document.getElementById('contact-info');
 
 
-    var box = document.getElementById('box');
-    box.addEventListener('click', function(){
-        box.style.transform = 'rotateX(180deg) translate(9px, 6px)';
-    })
+//     bioLink.addEventListener('click', function(){
+//         bioPage.style.display = 'flex';
+//         // homePage.style.display = 'none';
+//         workPage.style.display = 'none';
+//         contactPage.style.display = 'none';
+//     });
+//     workLink.addEventListener('click', function(){
+//         workPage.style.display = 'flex';
+//         homePage.style.display = 'none';
+//         bioPage.style.display = 'none';
+//         contactPage.style.display = 'none';
+//     });
+//     contactLink.addEventListener('click', function(){
+//         contactPage.style.display = 'flex';
+//         homePage.style.display = 'none';
+//         workPage.style.display = 'none';
+//         bioPage.style.display = 'none';
+//     });
+
+
+//     var box = document.getElementById('box');
+//     box.addEventListener('click', function(){
+//         box.style.transform = 'rotateX(180deg) translate(9px, 6px)';
+//     })
     
