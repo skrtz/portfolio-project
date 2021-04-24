5d1000da6e17087301edbab1f162b4ee506ea8fc
Stephen Kurtz <stephenjkurtz@gmail.com>
2021-04-24 15:55:25 -0400

    add web app content

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,35 +1,55 @@
-// var bioLink = document.getElementById('about-link');
-// var workLink = document.getElementById('work-link');
-// var contactLink = document.getElementById('contact-link');
+    // View Project Buttons
+    document.getElementById('app-1').addEventListener('click', function(){
+        document.getElementById('pic-1').style.transition = 'opacity 1s ease-in-out';
+        document.getElementById('pic-1').style.opacity = '80%';
+        document.getElementById('view-app-1').style.transition = 'opacity 1s ease-in-out';
+        document.getElementById('view-app-1').style.opacity = '80%';
+    })
 
-// var homePage = document.getElementById('home');
-// var bioPage = document.getElementById('about-me');
-// var workPage = document.getElementById('my-work')
-// var contactPage = document.getElementById('contact-info');
-
-
-//     bioLink.addEventListener('click', function(){
-//         bioPage.style.display = 'flex';
-//         // homePage.style.display = 'none';
-//         workPage.style.display = 'none';
-//         contactPage.style.display = 'none';
-//     });
-//     workLink.addEventListener('click', function(){
-//         workPage.style.display = 'flex';
-//         homePage.style.display = 'none';
-//         bioPage.style.display = 'none';
-//         contactPage.style.display = 'none';
-//     });
-//     contactLink.addEventListener('click', function(){
-//         contactPage.style.display = 'flex';
-//         homePage.style.display = 'none';
-//         workPage.style.display = 'none';
-//         bioPage.style.display = 'none';
-//     });
+    document.getElementById('app-2').addEventListener('click', function(){
+        document.getElementById('pic-2').style.transition = 'opacity 1s ease-in-out';
+        document.getElementById('pic-2').style.opacity = '80%';
+        document.getElementById('view-app-2').style.transition = 'opacity 1s ease-in-out';
+        document.getElementById('view-app-2').style.opacity = '80%';
+    })
 
 
-//     var box = document.getElementById('box');
-//     box.addEventListener('click', function(){
-//         box.style.transform = 'rotateX(180deg) translate(9px, 6px)';
-//     })
-    
+
+
+var bioLink = document.getElementById('about-link');
+var workLink = document.getElementById('work-link');
+var contactLink = document.getElementById('contact-link');
+
+var homePage = document.getElementById('home');
+var bioPage = document.getElementById('about-me');
+var workPage = document.getElementById('my-work')
+var contactPage = document.getElementById('contact-info');
+
+
+    // bioLink.addEventListener('click', function(){
+    //     bioPage.style.display = 'flex';
+    //     // homePage.style.display = 'none';
+    //     workPage.style.display = 'none';
+    //     contactPage.style.display = 'none';
+    // });
+    // workLink.addEventListener('click', function(){
+    //     workPage.style.display = 'flex';
+    //     homePage.style.display = 'none';
+    //     bioPage.style.display = 'none';
+    //     contactPage.style.display = 'none';
+    // });
+    // contactLink.addEventListener('click', function(){
+    //     contactPage.style.display = 'flex';
+    //     homePage.style.display = 'none';
+    //     workPage.style.display = 'none';
+    //     bioPage.style.display = 'none';
+    // });
+
+
+    var box = document.getElementById('box');
+    box.addEventListener('click', function(){
+        box.style.transform = 'rotateX(180deg) translate(9px, 6px)';
+
+    })
+
+
